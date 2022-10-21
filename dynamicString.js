7ef02d2a15ae340fecd892e83a368700046fe12d
erobinofficial <robiul8778@gmail.com>
2022-10-22 01:53:16 +0600

    dynamic string

--- dynamicString.js
+++ dynamicString.js
@@ -1,1 +1,8 @@
-console.log('string pornewala');
+const friends = ['abul', 'labil', 'munna', 'nahid'];
+const new1 = `<p class="friend">Friends-${friends.length}</p>`;
+console.log(new1);
+
+const first = 'Munna';
+const last = 'Hossain';
+const fullName = `${first} ${last}`;
+console.log(fullName);
