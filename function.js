c0afcc7559b7d9b3c04190cf95b2b9f7191521c3
erobinofficial <robiul8778@gmail.com>
2022-10-22 00:25:09 +0600

    function

--- function.js
+++ function.js
@@ -1,3 +1,13 @@
-function amiakhum(){
-    console.log('ami akhum');
+// function default parameter for not provided values
+function add(num1, num2 = 23) {
+    // option 1
+    // num2 = num2 || 0;
+    // option 2
+    // if(num2 == undefined){
+    //     num2 = 0;
+    // }
+    const total = num1 + num2;
+    return total;
 }
+const result = add(10);
+console.log(result)
